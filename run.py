077908dbee2cb9d2f3d82814f9045b5d3e3acc8b
Xiaoyun Zhang <609313700@qq.com>
2017-10-07 16:56:40 +0800

    update

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+'''
+自动生成README.md文件并使文件中的每一行生成一个在当前文件夹中以.md结尾的文件的文件名。
+'''
+
 import os
+import urllib.parse
+
+
 files = os.listdir()
-with open('README.md','w') as f:
-    for file in files:
-        if file.endswith('.md'):
-            f.write('* ['+file+']('+file+'/)\n')
+with open('README.md', 'w') as f:
+    for filename in files:
+        if filename.endswith('.md'):
+            encoded_filename = urllib.parse.quote(filename)
+            f.write('* [%s](%s/)\n' % (filename, encoded_filename))
